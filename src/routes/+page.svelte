d280af729ec5253226c59f1733a7b5fdf9adde9c
Mori Adrian Rosland <mori@live.no>
2023-09-03 19:42:14 +0200

    Added tailwindcss and daisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-green-400">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button class="btn btn-primary w-64 rounded-full">I'm a button</button>
