7ac717c6cb47005d3929a43c2d0a05771d482bb5
Mori Adrian Rosland <mori@live.no>
2023-09-04 21:11:37 +0200

    Added Hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,19 @@
-<div class="h-screen grid place-content-center">
-	<h1 class="font-mono text-5xl text-blue-600">Mori Adrian Rosland</h1>
-	<p class="font-mono text-xl text-red-500">
-		Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-	</p>
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-max">
+			<h1 class="text-5xl font-bold">Hello there👋 I'm Mori</h1>
+			<p class="py-6">
+				I study Electronic System Design and Innovation at NTNU, and is currently in my 3rd year.
+			</p>
+			<button class="btn btn-ghost btn-outline normal-case"
+				>Have a look at some of my projects🛠</button
+			>
+		</div>
+	</div>
 </div>
