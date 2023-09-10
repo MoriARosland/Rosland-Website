895089b06fae2a960b3e55ead3b6c1633fe09d53
Mori Adrian Rosland <mori@live.no>
2023-09-10 20:49:57 +0200

    Added temporary layout for main section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import card_image from '$lib/images/placeholder.jpg';
+	// import type { PageData } from './$types';
 
-	export let data: PageData;
+	// // export let data: PageData;
 </script>
 
 <div class="hero min-h-screen bg-base-200">
@@ -14,6 +15,35 @@
 			<button class="btn btn-ghost btn-outline normal-case"
 				>Have a look at some of my projects🛠</button
 			>
+		</div>
+	</div>
+</div>
+
+<div class="relative min-h-screen bg-zinc-500/90">
+	<div class="min-w-screen flex justify-center">
+		<div class="join py-2">
+			<button class="btn join-item btn-outline btn-info btn-sm">Projects🛠</button>
+			<button class="btn join-item btn-outline btn-info btn-sm">Bio💬</button>
+		</div>
+	</div>
+	<div class="flex px-3 py-2 justify-center">
+		<div class="card w-96 h-96 bg-base-100 shadow-xl m-3">
+			<figure>
+				<img src={card_image} alt="placeholder" />
+			</figure>
+			<div class="card-body">
+				<h2 class="card-title">Im a placeholder!</h2>
+				<p>This is just some text to PLACEHOLD.</p>
+			</div>
+		</div>
+		<div class="card w-96 h-96 bg-base-100 shadow-xl m-3">
+			<figure>
+				<img src={card_image} alt="placeholder" />
+			</figure>
+			<div class="card-body">
+				<h2 class="card-title">Im a placeholder!</h2>
+				<p>This is just some text to PLACEHOLD.</p>
+			</div>
 		</div>
 	</div>
 </div>
