991ebeba94c265f1cabb8f0ea157cfba12376dd0
Mori Adrian Rosland <mori@live.no>
2023-09-24 19:22:07 +0200

    Updated README

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -67,6 +67,18 @@
 							<spand class="italic"> (this card will be clickable with more info soon) </spand>
 						</p>
 					</div>
+					<dialog id="my_modal_1" class="modal">
+						<div class="modal-box">
+							<h3 class="font-bold text-lg">Hello!</h3>
+							<p class="py-4">Press ESC key or click the button below to close</p>
+							<div class="modal-action">
+								<form method="dialog">
+									<!-- if there is a button in form, it will close the modal -->
+									<button class="btn">Close</button>
+								</form>
+							</div>
+						</div>
+					</dialog>
 				</div>
 				<div class="card w-96 bg-neutral-200 shadow-xl m-3">
 					<figure>
