1e83119e649c47fabffeb437f201c03bb7db348b
Mori Adrian Rosland <mori@live.no>
2023-12-16 15:21:13 +0100

    Update Firebase configuration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,25 +1,27 @@
-import { initializeApp } from "firebase/app";
 // import { getStorage, getDownloadURL, ref } from "firebase/storage";
+import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, addDoc, type DocumentData } from "firebase/firestore";
-import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID } from '$env/static/private';
+import { getAuth } from "firebase/auth";
+import { PUBLIC_FIREBASE_API_KEY, PUBLIC_FIREBASE_AUTH_DOMAIN, PUBLIC_FIREBASE_PROJECT_ID, PUBLIC_FIREBASE_STORAGE_BUCKET, PUBLIC_FIREBASE_MESSAGING_SENDER_ID, PUBLIC_FIREBASE_APP_ID, PUBLIC_FIREBASE_MEASUREMENT_ID } from '$env/static/public';
 import type { Firebase_Project } from "./types";
 
 
 const firebaseConfig = {
-    apiKey: FIREBASE_API_KEY,
-    authDomain: FIREBASE_AUTH_DOMAIN,
-    projectId: FIREBASE_PROJECT_ID,
-    storageBucket: FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-    appId: FIREBASE_APP_ID,
-    measurementId: FIREBASE_MEASUREMENT_ID
+    apiKey: PUBLIC_FIREBASE_API_KEY,
+    authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: PUBLIC_FIREBASE_APP_ID,
+    measurementId: PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth();
 // const storage = getStorage(app);
 
 export async function getProjectsData() {
@@ -31,8 +33,6 @@ export async function getProjectsData() {
 
     querySnapshot.forEach((doc) => {
         projectList.push(doc.data());
-        // console.log(doc.data());
-        // console.log(`${doc.id} => ${doc.data()}`);
     });
 
     // // -- Get download reference [DEBUGGING] -- //
