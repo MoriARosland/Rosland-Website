7090e8c66265dd1500d7c363e2007a2213deb8ad
Mori Adrian Rosland <mori@live.no>
2023-12-18 12:14:22 +0100

    Refactor login functionality and add user authentication

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -1,15 +1,26 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-
 	import { auth } from '$lib/firebase';
 	import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+	import { userStore } from 'sveltefire';
 
 	async function signInWithGoogle() {
 		const provider = new GoogleAuthProvider();
 		const user = await signInWithPopup(auth, provider);
 		console.log(user);
 	}
+
+	const user = userStore(auth);
 </script>
 
-<h2>Login to view page.</h2>
-<button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
+{#if $user && $user.uid == '4NFoYv76eyTTPtptAD7rhcSnm4K2'}
+	// Remove this IdleDeadline.
+	<p>User id: {$user?.uid}</p>
+	<button class="btn btn-primary" on:click={() => signOut(auth)}>Sign out</button>
+{:else}
+	<div class="flex justify-center items-center min-h-screen">
+		<div class="text-center">
+			<h2 class="my-3">Login to view page.</h2>
+			<button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
+		</div>
+	</div>
+{/if}
