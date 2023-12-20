a7224c81e970db55d78469669ec559d9273f4f72
moriar <moriar@stud.ntnu.no>
2023-12-20 12:08:56 +0100

    Add POST request handler for verifying Google ID

--- src/routes/api/verifyID/+server.ts
+++ src/routes/api/verifyID/+server.ts
@@ -1,5 +1,22 @@
+import { error, json } from '@sveltejs/kit';
+import { GOOGLE_ID } from '$env/static/private'
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async () => {
-    return new Response();
+export const POST: RequestHandler = async ({request}) => {
+    let requestData;
+
+    try {
+        requestData = await request.json();
+    } catch (err) {
+        throw error(400, 'Invalid request.');
+    }
+
+    const { googleID } = requestData;
+    // console.log(`Received ID: ${googleID}`);
+
+    if (googleID == GOOGLE_ID) {
+        return json({isAdmin: true})
+    } else {
+        throw error(401, 'Login denied. You must be an admin to sign in');
+    }
 };
